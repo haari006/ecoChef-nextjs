6da8f2198d330d9098f957c25e1380425a91c7bd
Haariharan Rajakumar <harikk002@gmail.com>
2025-09-02 03:53:12 +0000

    use this image as background for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,18 @@ import RecipeGenerator from "@/components/recipe-generator";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4 py-8 md:py-12">
-      <RecipeGenerator />
+    <div 
+      className="relative min-h-[calc(100vh-57px)] flex items-center justify-center p-4"
+    >
+      <div 
+        className="absolute inset-0 bg-cover bg-center"
+        style={{backgroundImage: "url(/background.jpg)"}}
+      >
+        <div className="absolute inset-0 bg-black/50 backdrop-blur-sm" />
+      </div>
+      <div className="relative z-10 w-full">
+        <RecipeGenerator />
+      </div>
     </div>
   );
 }
